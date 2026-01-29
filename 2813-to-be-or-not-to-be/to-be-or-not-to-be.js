3fc81a8c1e0b4f903ed503fd0e19f35ce49881da
Mansur <100165031+M4nsur@users.noreply.github.com>
2026-01-29 19:38:36 +0300

    Time: 35 ms (92.96%) | Memory: 53 MB (92.18%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+   
+  return {
+    toBe: (otherVal)=> {
+        if (val === otherVal) {
+            return true
+        } throw "Not Equal";
+    },
+    notToBe:(otherVal)=> {
+        if (otherVal !== val ) {
+            return true
+        } throw "Equal"
+    }
+  }  
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
