a8fd78224cde1190e84d8e194266b68683ec8b5b
Mansur <100165031+M4nsur@users.noreply.github.com>
2026-03-07 01:17:32 +0300

    Time: 8 ms (51.76%) | Memory: 63.3 MB (13.17%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,18 +3,20 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let result = []
-    const n = nums.length - 1 
+    const n = nums.length - 1
+    let res = []
     let left = 0
     let right = n
+
     for(let i = n; i > -1; i--) {
-        if(nums[left] * nums[left] < nums[right] * nums[right]) {
-            result[i] =  nums[right] * nums[right]
-            right--
-        } else if (nums[left] * nums[left] >= nums[right] * nums[right]) {
-            result[i] = nums[left] * nums[left]
+        if(nums[left] * nums[left] >= nums[right] * nums[right]) {
+            res[i] = nums[left] * nums[left]
             left++
+        } else {
+            res[i] = nums[right] * nums[right]
+            right--
         }
     }
-    return result
+
+    return res
 };
