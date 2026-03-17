ec16d7c3cbefd47df2754e466e58d76a17642ac8
Mansur <100165031+M4nsur@users.noreply.github.com>
2026-03-18 00:37:57 +0300

    Time: 0 ms (100.00%) | Memory: 56.2 MB (17.63%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -3,13 +3,14 @@
  * @param {string} t
  * @return {boolean}
  */
+
 var isSubsequence = function(s, t) {
     let pointer = 0
 
-    for(let i = 0; i < t.length; i++) {
+    for(let i = 0; i < t.length ; i++) {
         if(s[pointer] === t[i]) {
             pointer++
         }
     }
-    return pointer === s.length
+        return pointer === s.length
 };
